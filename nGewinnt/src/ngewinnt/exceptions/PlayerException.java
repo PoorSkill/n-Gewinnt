ecddc70e3c660619de3700c792c4f32f57ee194d
Anton Kesy <anton@kesy.de>
2019-11-27 12:09:36 +0100

    PlayerExceptions

---
+++ nGewinnt/src/ngewinnt/exceptions/PlayerException.java
@@ -0,0 +1,25 @@
+/**
+ * 27.11.2019
+ */
+package ngewinnt.exceptions;
+
+/**
+ * @author Poor
+ *
+ */
+public class PlayerException extends Exception {
+
+	/**
+	 * generadted Serial
+	 */
+	private static final long serialVersionUID = -4880646768944844306L;
+
+	PlayerException() {
+
+	}
+
+	PlayerException(String message) {
+		super(message);
+	}
+
+}
